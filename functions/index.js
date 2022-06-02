59e795810bee99b29991da90617e85cb2b227810
supermanshuvo <shuvothesuperman@gmail.com>
2022-06-02 10:14:34 +0600

    Add some comment for better understanding

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,11 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
+/*
+*   For key.json you need to create it
+*   @ Project setting -> Service account -> Generate new private key button
+*   then rename it key.json or any name you want
+* */
 let serviceAccount = require("./key.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
