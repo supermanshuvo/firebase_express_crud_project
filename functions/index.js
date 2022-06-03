beaf6e44c02455858a7d5cf7aad492e50646cec6
supermanshuvo <shuvothesuperman@gmail.com>
2022-06-03 20:55:47 +0600

    function and hosting practice is done

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,16 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+exports.bigban = functions.https.onRequest((req,res)=>{
+    const hours =(new Date().getHours()%12)+6;
+    res.status(200).send(`
+    <!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'Superman '.repeat(hours)}
+    </body>
+  </html>
+    `);
+});
