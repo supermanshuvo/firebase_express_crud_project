3b2211b36e9edc128b78f76767f16e2c5d173ee5
supermanshuvo <shuvothesuperman@gmail.com>
2022-06-20 10:12:44 +0600

    functions add in index file

--- functions/index.js
+++ functions/index.js
@@ -13,3 +13,88 @@ const cors = require('cors');
 *   @ Project setting -> Service account -> Generate new private key button
 *   then rename it key.json or any name you want
 * */
+let serviceAccount = require('./key.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore(); // database just added
+const app = express(); // app now initialized
+app.use(cors({origin:true}));
+
+// Read All data from api/read/
+app.get('/api/read', async  (req,res)=>{
+  try{
+    const document = db.collection('userInfo');
+    const response = [];
+    await document.get().then(Snapshot=>{
+      let docs = Snapshot.docs;
+      for(doc of docs){
+        const selectedItem = {
+          id:doc.id,
+          name:doc.data().name,
+          mobile:doc.data().mobile,
+          address:doc.data().address,
+        }
+        response.push(selectedItem);
+      }
+      return response;
+    });
+    return res.status(200).send(response);
+  }catch (error){
+    return res.status(200).send({status:"Failed",msg:"Failed to get"});
+  }
+});
+
+// post Data in user info
+app.post("/api/create",async (req,res)=>{
+  try{
+    await db.collection('userInfo').doc(`/${Date.now()}`).create({
+      id: Date.now(),
+      name:req.body.name,
+      mobile: req.body.mobile,
+      address : req.body.address,
+    });
+    return res.status(200).send({status:"Success",msg:"Data saved"});
+  }catch (error){
+    return res.status(200).send({status:"Failed",msg:"Data not saved"});
+  }
+});
+
+// Read specific data
+app.get('/api/read/:id', async  (req,res)=>{
+  try{
+    const document = await db.collection('userInfo').doc(req.params.id).get();
+    const response = document.data();
+    // console.log(response);
+    return res.status(200).send(response);
+  }catch (error){
+    return res.status(200).send({status:"Failed",msg:"Failed to get"});
+  }
+});
+
+// update specific data
+app.put('/api/update/:id', async  (req,res)=>{
+  try{
+    const document  = await db.collection('userInfo').doc(req.params.id).update({
+      name:req.body.name,
+      mobile: req.body.mobile,
+      address : req.body.address,
+    });
+    return res.status(200).send("Data Updated");
+  }catch (error){
+    return res.status(200).send({status:"Failed",msg:"Failed to Update"});
+  }
+});
+
+// Delete Data from user info
+
+app.delete("/api/delete/:id", async (req,res)=>{
+  try{
+    const document = await db.collection('userInfo').doc(req.params.id).delete();
+    return  res.status(200).send("Data is deleted");
+  }catch (error){
+    return res.status(200).send({status:"Failed",msg:"Failed to Delete"});
+  }
+});
+exports.app = functions.https.onRequest(app);
